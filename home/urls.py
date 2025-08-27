cbeb16d7407531bf779394d2a93dfa0b80753b5e
Iván de León <idl@t2mc.net>
2025-08-27 09:04:57 -0600

    add new views

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView,VentaView
+from .views import HomeView,VentaView, bienvenida,contacto,about
 
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='homeapp'),
     path('ventas/', VentaView.as_view(), name='ventasapp'),
+    path('bienvenida/', bienvenida, name='bienvenida'),
+    path('contacto/<str:correo>', contacto, name='contacto'),
+    path('about/', about, name='about'),
     # Include other app URLs here
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse 
+from django.contrib.auth.decorators import login_required   
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -8,3 +10,18 @@ class HomeView(TemplateView):
 class VentaView(TemplateView):
     template_name = 'ventas.html'
 
+@login_required
+def bienvenida(request):
+    if request.method == 'GET':
+        return HttpResponse("Bienvenido a la página principal")
+    elif request.method == 'PUT':
+        return HttpResponse("Bienvenido a la página principal - POST")
+    else:
+        return HttpResponse("Método no permitido", status=405)
+
+
+def contacto(request,correo):
+    return HttpResponse("Se  enviará un correo a la dirección: "+correo)  
+
+def about(request):
+    return render(request, 'home.html')
