9ec46ac3d1c9781bd295fd06d64bff1844ea6803
Iván de León <idl@t2mc.net>
2025-08-28 09:41:31 -0600

    1. Add test classes 2. Update categoria_list.html for show data

--- apps/products/tests.py
+++ apps/products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Categoria, Medida
+from django.urls import reverse
+
+class CategoriaListViewTestCase(TestCase):
+    def setUp(self):
+        self.categoria = Categoria.objects.create(categoria='Categoria Teste', habilitado=True, es_producto=True)
+        self.categoria = Categoria.objects.create(categoria='Reactivos', habilitado=True, es_producto=True)
+
+    def test_categoria_count(self):
+        self.assertEqual(Categoria.objects.count(), 2)
